de042463cea411fbe9f4e168104a6deefaad2413
DeGambino <126100679+DeGambino@users.noreply.github.com>
2025-04-18 23:54:21 +0200

    Update authentication configuration: Change sign-in page path from "/login" to "/admin/login" and adjust authorization logic to redirect to "/admin" instead of "/dashboard" for improved admin access control.

--- auth.config.ts
+++ auth.config.ts
@@ -2,17 +2,17 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   pages: {
-    signIn: "/login",
+    signIn: "/admin/login",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnDashboard = nextUrl.pathname.startsWith("/admin");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return Response.redirect(new URL("/admin", nextUrl));
       }
       return true;
     },
